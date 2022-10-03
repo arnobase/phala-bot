05cb8ec29d3fb6827c2b77e7f94763bce72c8d6b
arnobase <arnaud.gardies@gmail.com>
2022-10-03 17:35:28 +0200

    nettoyage

--- .history/index_20221003110012.js
+++
@@ -1,8 +0,0 @@
-import { ApiPromise, WsProvider } from '@polkadot/api';
-
-// Construct
-const wsProvider = new WsProvider('wss://public-rpc.pinknode.io/khala');
-const api = await ApiPromise.create({ provider: wsProvider });
-
-// Do something
-console.log(api.genesisHash.toHex());
