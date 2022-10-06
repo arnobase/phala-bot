a0de5bf9e5c89beb4939b2de400d153362f41b83
Arno <arnobaze@gmail.com>
2022-10-06 19:50:35 +0200

    v1

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 const argv = require('yargs/yargs')(process.argv.slice(2))
     .alias('i', 'info')
     .alias('c', 'claim')
@@ -7,15 +8,12 @@ const argv = require('yargs/yargs')(process.argv.slice(2))
     .alias('d', 'debug')
     .argv;
 
-/*
-https://github.com/Phala-Network/phala-blockchain/blob/68e8994d1517c6253e9330ab903fe76fac72df2d/scripts/js/bugfix-tools/dumpLockChart.js#L30
-*/
-
+const util = require('util');
 const { ApiPromise, WsProvider, Keyring } = require('@polkadot/api');
 const utils = require('./utils.js');
 const config = require('./config.js');
 const keyring = new Keyring({ type: 'sr25519' });
-const tx_end = false;
+const { phalaTypes } = require ( '@phala/typedefs');
 
 let pool_status = {
     cap: 0,
@@ -23,15 +21,46 @@ let pool_status = {
     free: 0,
     totalShares: 0,
     totalStake: 0,
+    rewardAcc: 0,
     stakable: 0
 };
 let staker_status = {
     free: 0,
     frozen: 0,
+    lockedInPool: 0,
     total: 0,
+    shares: 0,
+    availableRewards: 0,
+    rewardDebt: 0
 };
 
+async function signAndSend(tx,account) {
+    /* https://github.com/Phala-Network/phala-blockchain/blob/68e8994d1517c6253e9330ab903fe76fac72df2d/scripts/js/bugfix-tools/dumpLockChart.js#L30 */
+    let exit_status = 0;
+    const unsub = await tx.signAndSend(account, ({ events = [], status, txHash }) => {
+        if (status.isInBlock) {
+            console.log('Successful \n\tin block '+ status.asInBlock.toHex() + "\n\ttx hash: "+txHash);
+        } else {
+            console.log('Status of tx: ' + status.type);
+        }
+        events.forEach(({ phase, event: { data, method, section } }) => {
+            console.log("\tExtrinsic: "+phase.toString() + ' : ' + section + '.' + method + ' ' + data.toString());
+            exit_status = (method === "ExtrinsicFailed" ? 1 : 0);
+        });
+        if (status.isFinalized) {
+            console.log("\nexit_status: "+exit_status)
+            process.exit(exit_status);
+        }
+        setTimeout(() => {
+            console.log('No response in 60 sec... exiting');
+            process.exit(1);
+        }, 60000);
+    });
+    return unsub;
+}
+
 async function checkStatus(api) {
+    // First, get info about the pool
     let poolInfo = await api.query.phalaStakePool.stakePools(config.pool_pid);
     if (poolInfo.isSome) {
         poolInfo = poolInfo.unwrap();
@@ -46,11 +75,13 @@ async function checkStatus(api) {
             free: utils.pha(poolInfo.freeStake),
             totalShares: utils.pha(poolInfo.totalShares),
             totalStake: utils.pha(poolInfo.totalStake),
+            rewardAcc: BigInt(poolInfo.rewardAcc),
             stakable: pool_status.cap - utils.pha(poolInfo.totalStake)
         };
         console.log("POOL Status :")
         console.log(pool_status);
     }
+    // secondly, get info about the staking account
     const STAKER = keyring.addFromUri(config.staker_seed);
     keyring.setSS58Format(30);
     const staker_address = STAKER.address;
@@ -61,12 +92,28 @@ async function checkStatus(api) {
         frozen: utils.pha(stakerInfo.data.miscFrozen),
         total: utils.pha(stakerInfo.data.free),
     };
+    // Finaly, get info about the staking into the pool (to get the rewards amount)
+    let stakeInPoolInfo = await api.query.phalaStakePool.poolStakers([config.pool_pid ,staker_address]);
+    if (stakeInPoolInfo.isSome) {
+        stakeInPoolInfo = stakeInPoolInfo.unwrap()
+    }
+    staker_status = {
+        ...staker_status,
+        shares: BigInt(stakeInPoolInfo.shares),
+        availableRewards: BigInt(stakeInPoolInfo.availableRewards),
+        rewardDebt: BigInt(stakeInPoolInfo.rewardDebt),
+        lockedInPool: utils.pha(stakeInPoolInfo.locked)
+    }
+    console.log(staker_status.shares,pool_status.rewardAcc,staker_status.rewardDebt,staker_status.availableRewards)
+    const pendingRewards = (staker_status.shares*pool_status.rewardAcc) - staker_status.rewardDebt + staker_status.availableRewards
+    staker_status.pendingRewards = pendingRewards
     console.log("STAKER Status :")
     console.log(staker_status)
-    
+
     const pool_stakable = pool_status.stakable;
     const staker_free = staker_status.free
-    if ( staker_free <= pool_stakable) {
+    
+    if ( pendingRewards > 0 && staker_free <= pool_stakable) {
         const stake_amount = staker_free-1
         console.log("\nSTAKABLE amount : "+stake_amount * 1e9)
     }
@@ -86,25 +133,7 @@ async function claimStakingRewards(api) {
 
     if (!argv.debug) {
         const tx = await api.tx.phalaStakePool.claimRewards(pool_id, staker_address)
-        const unsub = await tx.signAndSend(STAKER, ({ events = [], status, txHash }) => {
-            if (status.isInBlock) {
-                console.log('Successful \n\tin block '+ status.asInBlock.toHex() + "\n\ttx hash: "+txHash);
-            } else {
-                console.log('Status of tx: ' + status.type);
-            }
-            events.forEach(({ phase, event: { data, method, section } }) => {
-                console.log("\tExtrinsic: "+phase.toString() + ' : ' + section + '.' + method + ' ' + data.toString());
-            });
-            if (status.isFinalized) {
-                //unsub();
-                process.exit();
-            }
-            setTimeout(() => {
-                //unsubscribe();
-                console.log('No response in 60 sec... exiting');
-                process.exit();
-            }, 60000);
-        });
+        const unsub = signAndSend(tx,STAKER);
     }
 }
 
@@ -114,44 +143,30 @@ async function stakeFreeAmount(api) {
 
     const pool_stakable = pool_status.stakable;
     const staker_free = staker_status.free
-    let stake_amount=0
-    if ( staker_free <= pool_stakable) {
-        stake_amount = (staker_free-1)
+   
+    let stake_amount = (staker_free-1)
+    if ( stake_amount > 0 && staker_free <= pool_stakable) {
+        
         // Si on passe un chiffre en param de stake, on vérifie qu'il est inférieur aux max free sur la pool
         if (typeof argv.stake === "number") {
             if (argv.stake > staker_free-1) {
-                stake_amount=staker_free-1
+                stake_amount = staker_free-1
             }
             else {
                 stake_amount=argv.stake
             }
         }
     }
+    else {
+        console.log("rien à stake")
+        process.exit(1)
+    }
     
-    console.log(typeof argv.stake === "number")
     console.log("stakeamount: "+stake_amount)
 
-    if (!argv.debug) {
+    if (!argv.debug && stake_amount > 0) {
         const tx = await api.tx.phalaStakePool.contribute(pool_id, stake_amount*1e12)
-        const unsub = await tx.signAndSend(STAKER, ({ events = [], status, txHash }) => {
-            if (status.isInBlock) {
-                console.log('Successful \n\tin block '+ status.asInBlock.toHex() + "\n\ttx hash: "+txHash);
-            } else {
-                console.log('Status of tx: ' + status.type);
-            }
-            events.forEach(({ phase, event: { data, method, section } }) => {
-                console.log("\tExtrinsic: "+phase.toString() + ' : ' + section + '.' + method + ' ' + data.toString());
-            });
-            if (status.isFinalized) {
-                //unsub();
-                process.exit();
-            }
-            setTimeout(() => {
-                //unsubscribe();
-                console.log('No response in 60 sec... exiting');
-                process.exit();
-            }, 60000);
-        });
+        const unsub = signAndSend(tx,STAKER);
     }
 }
 
@@ -164,46 +179,27 @@ async function restartMining(api) {
     console.log(totalStake)
     if (!argv.debug) {
         const tx = await api.tx.phalaStakePool.restartMining(pool_id, worker, totalStake)
-        const unsub = await tx.signAndSend(POOLOWNER, ({ events = [], status, txHash }) => {
-            if (status.isInBlock) {
-                console.log('Successful \n\tin block '+ status.asInBlock.toHex() + "\n\ttx hash: "+txHash);
-            } else {
-                console.log('Status of tx: ' + status.type);
-            }
-            events.forEach(({ phase, event: { data, method, section } }) => {
-                console.log("\tExtrinsic: "+phase.toString() + ' : ' + section + '.' + method + ' ' + data.toString());
-            });
-            if (status.isFinalized) {
-                //unsub();
-                process.exit();
-            }
-            setTimeout(() => {
-                //unsubscribe();
-                console.log('No response in 60 sec... exiting');
-                process.exit();
-            }, 60000);
-        });
+        const unsub = signAndSend(tx,POOLOWNER);
     }
 }
 
 (async () => {
     const wsProvider = new WsProvider('wss://public-rpc.pinknode.io/khala');
-    const api = await ApiPromise.create({ provider: wsProvider });
+    const api = await ApiPromise.create({ provider: wsProvider, types: phalaTypes });
     await checkStatus(api);
     let kill=true;
     if (argv.claim) {
-        console.log("\nclaim rewards")
+        console.log("\n## Claim rewards")
         await claimStakingRewards(api);
         kill=false;
     }
     if (argv.stake) {
-        console.log("\nstake rewards")
+        console.log("\n## Stake rewards")
         await stakeFreeAmount(api);
-
         kill=false
     }
     if (argv.restart) {
-        console.log("\nrestart mining")
+        console.log("\n## Restart mining")
         await restartMining(api);
         kill=false
     }
